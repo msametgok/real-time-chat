ab0c9a78c0be0b4bf0d862ef2e6d43d2ef3d3469
Mehmet Samet Gok <msametgok@hotmail.com>
2025-03-17 16:22:38 -0400

    DB connection in server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const http = require('http')
 const app = require('./app')
+const connectDB = require('./config/db')
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
+// Connect to MongoDB
+connectDB();
+
+// Create an HTTP server to use WebSocket
 const server = http.createServer(app);
 
 server.listen(PORT, ()=> {
