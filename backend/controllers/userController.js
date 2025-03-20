60a85017a74bee2110f7e9d4227b6030f4d6d15e
Mehmet Samet Gok <msametgok@hotmail.com>
2025-03-20 01:15:54 -0400

    user controller is created with getting user profile and updating user profile

---
+++ backend/controllers/userController.js
@@ -0,0 +1,34 @@
+const User = require('../models/User');
+
+// Get user profile
+exports.getUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.userId).select('-password') // Exclude password
+        if(!user) return res.status(404).json({message: 'User not found'});
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({message: 'Server error'})
+    }
+}
+
+// Update user profile
+exports.updateUserProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.userId);
+        if(!user) return res.status(404).json({message: 'User not found' });
+
+        const {username, email, password} = req.body;
+
+        if(username) user.username = username;
+        if(email) user.email = email;
+        if(password) {
+            const bcrypt = require('bcryptjs');
+            user.password = await bcrypt.hash(password,10);
+        }
+
+        await user.save();
+        res.json({message: 'Profile updated successfully'})
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+}
