b9bb6a8780f60252917e2dbcea16234f84e619f1
Mehmet Samet Gok <msametgok@hotmail.com>
2025-03-20 01:17:05 -0400

    user Route is added to app

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 module.exports = app;
