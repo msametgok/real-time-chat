7e49a2fc14bed645ee62f690711b2a5852d5da44
Mehmet Samet Gok <msametgok@hotmail.com>
2025-03-21 03:55:40 -0400

    bug fix

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -2,13 +2,12 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const verifyToken = (req, res, next) => {
-    const token = req.header('Authorization').split(' ')[1];;
-
-    if(!token) {
-        return res.status(401).json({message: 'Access Denied: No token provided'})
-    }
-
     try {
+        const token = req.header('Authorization').split(' ')[1];;
+
+        if(!token) {
+            return res.status(401).json({message: 'Access Denied: No token provided'})
+        }
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
