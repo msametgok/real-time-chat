6c22469fdb74a0a8faf09903ec640f3c0f0d0b62
Mehmet Samet Gok <msametgok@hotmail.com>
2025-04-20 23:22:52 -0400

    User search endpoint added to route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const {getUserProfile, updateUserProfile} = require('../controllers/userController');
+const {getUserProfile, updateUserProfile, searchUsers} = require('../controllers/userController');
 const verifyToken = require('../config/auth');
 
 const router = express.Router();
 
 router.get('/profile', verifyToken, getUserProfile);
 router.put('/profile', verifyToken, updateUserProfile);
+router.get('/', verifyToken, searchUsers);
 
 module.exports = router;
