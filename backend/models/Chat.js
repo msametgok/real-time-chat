4d2933f87ba0b1c66f0877c39f508bfa8eff0278
Mehmet Samet Gok <msametgok@hotmail.com>
2025-05-16 14:35:04 -0400

    Avatar, index and pre validate added to chat model

--- backend/models/Chat.js
+++ backend/models/Chat.js
@@ -4,31 +4,49 @@ const chatSchema = new mongoose.Schema({
     participants: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-        required: true,
-        unique: true
+        required: true
     }],
     chatName: {
         type: String,
-        trim: true
+        trim: true,
+        required: function() { return this.isGroupChat; }
     },
     isGroupChat: {
         type: Boolean,
+        required: true,
         default: false
     },
     latestMessage: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Message'
+        ref: 'Message',
+        default: null,
     },
     groupAdmin: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User',
+        required: function() { return this.isGroupChat; }
     },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
+    groupAvatarUrl: {
+        type: String,
+        trim: true,
+        default: null
+    }
 }, { timestamps: true 
 
-});        
+});
+
+chatSchema.index({ participants: 1 });
+chatSchema.index({ isGroupChat: 1, participants: 1 });
+chatSchema.index({ isGroupChat: 1, updatedAt: -1 });
+
+chatSchema.pre('validate', function(next) {
+    if (!this.isGroupChat && this.participants.length !== 2) {
+        return next(new Error('1-on-1 chats must have exactly two participants.'));
+    }
+    if (this.isGroupChat && this.participants.length < 2) { // Groups typically need at least 2, creator + 1 other
+        return next(new Error('Group chats must have at least two participants.'));
+    }
+    next();
+});
 
 module.exports = mongoose.model('Chat', chatSchema);
