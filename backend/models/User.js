6a8e89e3c87198e09d14e9d40fcd0618e6975a5e
Mehmet Samet Gok <msametgok@hotmail.com>
2025-05-16 14:31:17 -0400

    Hashing password move to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,25 +1,65 @@
+// models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
+        required: [true, 'Username is required.'], // Still good to have a message
         unique: true,
         trim: true
+        // minlength and maxlength can be primarily handled by express-validator
     },
     email: {
         type: String,
-        required: true,
+        required: [true, 'Email is required.'],
         unique: true,
         trim: true,
         lowercase: true
+        // Email format (match) can be primarily handled by express-validator
     },
     password: {
         type: String,
-        required: true
+        required: [true, 'Password is required.']
+        // Password minlength can be primarily handled by express-validator
+    },
+    avatar: {
+        type: String,
+        default: null
+    },
+    onlineStatus: {
+        type: String,
+        enum: ['online', 'offline'], // Enum validation is good at schema level
+        default: 'offline'
+    },
+    lastSeen: {
+        type: Date,
+        default: Date.now
     }
 }, {
     timestamps: true
 });
 
-module.exports = mongoose.model('User', userSchema)
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// INSTANCE METHOD: Compare Password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password);
+    } catch (error) {
+        throw error;
+    }
+};
+
+module.exports = mongoose.model('User', userSchema);
