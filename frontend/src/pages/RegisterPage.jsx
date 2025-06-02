234d7540f2039ab73cd1f4e4882777a8ddf3e4dd
Mehmet Samet Gok <msametgok@hotmail.com>
2025-06-01 23:29:00 -0400

    register page including form

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RegisterForm from '../components/auth/RegisterForm';
+
+function RegisterPage() { 
+    return (
+        <>
+            <RegisterForm />
+        </>
+    ) 
+}
+export default RegisterPage;
