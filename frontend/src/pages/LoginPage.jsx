3ce21a91f5d7256647067fd962549ca4ee98e141
Mehmet Samet Gok <msametgok@hotmail.com>
2025-06-01 23:29:30 -0400

    login page inluding form

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoginForm from '../components/auth/LoginForm';
+
+function LoginPage() { 
+    return (
+        <>
+            <LoginForm />
+        </>
+    )
+}
+export default LoginPage;
