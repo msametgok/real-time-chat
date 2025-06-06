f30963971c5aba87deeb93fdf96cbfdf2efb9b37
Mehmet Samet Gok <msametgok@hotmail.com>
2025-06-06 12:56:04 -0400

    removed css from main layout, only wraps children now

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+
 function MainLayout({ children }) {
   return (
     <div>
-      <header className="bg-blue-600 text-white p-4">Main App Header</header>
-      <main className="p-4">{children}</main> {/* ChatPage will render here */}
+      {children}
     </div>
   );
 }
