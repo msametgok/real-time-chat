d4e0597756ade25418c41b3822778520c869e80e
Mehmet Samet Gok <msametgok@hotmail.com>
2025-06-15 00:46:42 -0400

    main react dom starting point

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import { ChatProvider } from './contexts/ChatContext';
+import './index.css'; // Your global styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <App />
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <ChatProvider>
+          <App />
+        </ChatProvider>
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
